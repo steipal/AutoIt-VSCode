076f0789b18685094280d43ffeebac9fa095ffd8
steipal <steipal@gmail.com>
2024-01-01 21:54:46 +0100

    Add Do/Until to increase and decrease indent pattern.

--- src/languageConfiguration.js
+++ src/languageConfiguration.js
@@ -1,7 +1,7 @@
 const languageConfiguration = {
   indentationRules: {
-    increaseIndentPattern: /^\s*(For|Func|If|ElseIf|Else|Select|While|Case|Switch|With)\b/i,
-    decreaseIndentPattern: /^\s*(Next|EndFunc|EndIf|ElseIf|Else|EndSelect|EndSwitch|WEnd|EndWith)\b/i,
+    increaseIndentPattern: /^\s*(For|Func|If|ElseIf|Else|Select|While|Case|Switch|With|Do)\b/i,
+    decreaseIndentPattern: /^\s*(Next|EndFunc|EndIf|ElseIf|Else|EndSelect|EndSwitch|WEnd|EndWith|Until)\b/i,
     unIndentedLinePattern: /^\s*((;|#include).*)?$/,
   },
 };
